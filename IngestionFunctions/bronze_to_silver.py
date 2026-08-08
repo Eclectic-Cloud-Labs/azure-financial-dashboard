78e7a52b26419936e11292601430224ee8cc6cab
Gurvir-01 <gurvir-k@hotmail.com>
2026-08-08 16:25:32 -0600

    Bronze_to_silver transformation and cleaning complete
    Ensured the timer trigger for this new function is registered on functionapp by deploying to it. bronze_to_silver needed to be imported into the main .py file (function_app) as the runtime reads directly to that. cleaned data confirmed to land in silver. timer trigger should work, any updates on if it does or not will be covered next commits. Needed to use a buffer for the .to_parquet function which was tough but makes sense after researching. All data is cleaned and ready to be trasnformed once again into gold

--- IngestionFunctions/bronze_to_silver.py
+++ IngestionFunctions/bronze_to_silver.py
@@ -1,28 +1,66 @@
 from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import azure.functions as func
+import json
+import pandas as pd
+import io
+from function_app import app
 
-containerName = "bronze"
-blobName = "AlphaVantage"
+bronzeContainer = "bronze"
+silverContainer = "silver"
 credential = DefaultAzureCredential()
 accountUrl = "https://gurbostorage.blob.core.windows.net"
 
 bsc =  BlobServiceClient(credential=credential, account_url=accountUrl)
-container_client = bsc.get_container_client(container=containerName)
-blob_client = container_client.get_blob_client(blob=blobName)
-blob_client = bsc.get_blob_client(container=containerName, blob="AlphaVantage")
 
-# @app.timer_trigger(schedule="0 15 21 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
+
+@app.timer_trigger(schedule="0 15 21 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
 def bronze_to_silver(myTimer: func.TimerRequest) -> None:
+    
+    container_client = bsc.get_container_client(container=bronzeContainer)
     blobs = container_client.list_blobs()
     newBlob = None
+    newBlob_date = None
+    
+    # looks for the newest blob file for most recent reports
     for blob in blobs:
         if newBlob is None or blob.last_modified > newBlob.last_modified:
             newBlob = blob
+            newBlob_date = blob.last_modified
     
+    df = transform(newBlob, newBlob_date, container_client)
+    sendToSilver(df)
+
+
+##HELPER FUNCTIONS##
+# cleans raw data from Time Series (Daily) key, add new titles, make all applicable values into float types, index/date column turns into real datetime obj's, and returns df for next function to use 
+def transform(newBlob, newBlob_date, container_client):
+    print(newBlob_date)
     data = container_client.download_blob(newBlob).readall().decode("utf-8")
-    print(data)
+    data = json.loads(data) 
+    data = data["Time Series (Daily)"]
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df.rename(columns={"1. open": "Open", "2. high": "High", "3. low": "Low", "4. close": "Close", "5. volume": "Volume",}, inplace=True)
+    df = df.astype(float)
+    df.index = pd.to_datetime(df.index)
+    df.index.name = "IBM"
+    return df
+
+# sends parquet file to silver storage 
+def sendToSilver(df):
+    # create in memory byte file location
+    buffer = io.BytesIO()
+    
+    # creates parquet file at in memory buffer location. Pointer ends at the end of the file, so seek resets it back to the beginning
+    df.to_parquet(buffer, engine="pyarrow")
+    buffer.seek(0)
+    
+    # get silver + upload
+    silver_Container_client = bsc.get_container_client(container=silverContainer)
+    silver_Container_client.upload_blob(name="AlphaVantage/ohlc.parquet", data=buffer, overwrite=True)
 
 
-if __name__ == "__main__":
-    bronze_to_silver(None)
+
+##FOR LOCAL TESTING##
+# if __name__ == "__main__":
+#     bronze_to_silver(None)
--- IngestionFunctions/function_app.py
+++ IngestionFunctions/function_app.py
@@ -7,6 +7,9 @@ from datetime import datetime
 from azure.storage.blob import BlobServiceClient
 app = func.FunctionApp()
 
+# runtime detection
+from bronze_to_silver import bronze_to_silver
+
 # Azure function app sees this and retains when the function is supossed to run based on the schedule
 # Function brings in "credential" to authenticate identity (used to get key vault secret for Alpha vantage API, )
 @app.timer_trigger(schedule="0 0 21 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
@@ -20,6 +23,8 @@ def AlphaVantageIngest(myTimer: func.TimerRequest) -> None:
 # API call to Alpha Vantage, creates clients to access blob storage 
 def upload_blob(secret, credential):
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={secret.value}"
+    # for later stock symbol gets
+    urlData = {"url": "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={secret.value}"}
     r = requests.get(url)
     # parse to Json 
     data = json.dumps(r.json())
@@ -27,10 +32,12 @@ def upload_blob(secret, credential):
     
     accountUrl = "https://gurbostorage.blob.core.windows.net"
     
+    # upload to bronze storage
     bsc = BlobServiceClient(account_url=accountUrl, credential=credential)
     container_client = bsc.get_container_client(container="bronze")
     container_client.upload_blob(name=filename, data=data)
 
-    
+
+##FOR LOCAL TESTING##
 # if __name__ == "__main__":
 #     AlphaVantageIngest(None)
